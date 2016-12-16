ca78b9fdf317dc8a712e5d48e56bba10d411fa09
Mark Joseph Ronquillo <markronquillo23@gmail.com>
2016-12-16 18:54:56 +0800

    posts

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def index(request):
-	context = {}
+from .models import Post
+
+def post_create(request):
+	return HttpResponse('<h1> Create </h1>')
+
+def post_detail(request, slug):
+	return HttpResponse("<h1> {0} </h1>".format(slug))
+
+def post_update(request, slug):
+	return HttpResponse('<h1> Update </h1>')
+	
+def post_delete(request, slug):
+	return HttpResponse('<h1> Delete </h1>')
+	
+def post_list(request):
+	today = timezone.now().date()
+	queryset_list = Post.objects.active()
+	context = {
+		'posts': Post.objects.all()
+	}
 	return render(request, 'posts/index.html', context)
